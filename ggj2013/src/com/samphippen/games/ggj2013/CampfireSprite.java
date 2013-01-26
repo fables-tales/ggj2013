e817b2f0016ad54473aee02af7a0fd92906258af
Sam Phippen <samphippen@googlemail.com>
2013-01-26 20:29:49 +0000

    Campfire all the things!
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- ggj2013/src/com/samphippen/games/ggj2013/CampfireSprite.java
+++ ggj2013/src/com/samphippen/games/ggj2013/CampfireSprite.java
@@ -1,5 +1,8 @@
 package com.samphippen.games.ggj2013;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -8,13 +11,31 @@ public class CampfireSprite implements GameObject {
 
     private Sprite mOnSprite;
     private Sprite mOffSprite;
+    
+    private List<Sprite> mOnSprites = new ArrayList<Sprite>();
+    
+    private List<Sprite> mOffSprites = new ArrayList<Sprite>();
 
     private boolean mOn = true;
     private Vector2 mPosition;
+    private int count = 0;
 
     public CampfireSprite() {
-        mOnSprite = GameServices.loadSprite("campfire.png");
-        mOffSprite = GameServices.loadSprite("campfire-off.png");
+        loadFrames(mOnSprites, 24, "campfire_large_", ".png");
+        loadFrames(mOffSprites, 20, "campfire_small_", ".png");
+    }
+    
+    public void loadFrames(List<Sprite> out, int count, String prefix, String suffix) {
+        for (int i = 0; i < count; i++) {
+            String filename = "" + i;
+            while (filename.length() < 3) {
+                filename = "0" + filename;
+            }
+
+            filename = prefix + filename + suffix;
+            Sprite s = GameServices.loadSprite(filename);
+            out.add(s);
+        }
     }
 
     public void setPosition(Vector2 pos) {
@@ -33,6 +54,9 @@ public class CampfireSprite implements GameObject {
 
     @Override
     public void update() {
+        mFrameCount++;
+        mOnSprite = mOnSprites.get(mFrameCount % mOnSprites.size());
+        mOffSprite = mOffSprites.get(mFrameCount % mOffSprites.size());
         if (PlayerObject.getInstance().getPosition().dst(mPosition) < 40) {
             System.out.println("off!");
             mOn = false;

fe8077cc2888b447a41acd6162ed2b5c3dc65d83
Jenny Bartle <jgbartle@gmail.com>
2013-01-26 08:16:59 +0000

    Add constants :O

---
+++ ggj2013-desktop/src/com/samphippen/games/ggj2013/Constants.java
@@ -0,0 +1,7 @@
+package com.samphippen.games.ggj2013;
+
+public class Constants {
+	static double SIZE = 0;
+	static double SPEED = 0;
+
+}
--- ggj2013-desktop/src/com/samphippen/games/ggj2013/Main.java
+++ ggj2013-desktop/src/com/samphippen/games/ggj2013/Main.java
@@ -1,9 +1,14 @@
 package com.samphippen.games.ggj2013;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
+	
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "ggj2013";
@@ -12,5 +17,20 @@ public class Main {
 		cfg.height = 600;
 		
 		new LwjglApplication(new GameHolder(), cfg);
+		
+		setConstants();
+	}
+	
+	private static void setConstants(){
+		File constantsFile = new File(System.getProperty("user.dir") + "/Constants.txt");
+		System.out.println(constantsFile.getAbsolutePath());
+		FileInputStream fileStream;
+		try {
+			fileStream = new FileInputStream(constantsFile);
+			ConstantsReader constantsReader = new ConstantsReader();
+			constantsReader.readConstants(fileStream);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }

504103a6130d7b9982a7f8fd73ab35c7912a4fae
Sam Phippen <samphippen@googlemail.com>
2013-01-26 15:23:20 +0000

    Do it
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ make-game-zip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+dir=`mktemp -d lolwatXXXXXX`
+curdir=`pwd`
+echo $dir
+mkdir $dir/ggj2013
+cp -r ggj2013* $dir/ggj2013/
+cp -r *.bat $dir/ggj2013/
+cp -r *.command $dir/ggj2013/
+cd $dir
+zip -r ggj2013.zip ggj2013
+mv ggj2013.zip $curdir
+cd $curdir
+rm -rf $dir

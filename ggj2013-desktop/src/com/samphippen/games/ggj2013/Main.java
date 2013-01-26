0baba283bde233449933e4d1bba66e0b7812e38f
Alastair Lynn <arplynn@gmail.com>
2013-01-26 08:33:57 +0000

    Clean up Main.java

--- ggj2013-desktop/src/com/samphippen/games/ggj2013/Main.java
+++ ggj2013-desktop/src/com/samphippen/games/ggj2013/Main.java
@@ -1,26 +1,17 @@
 package com.samphippen.games.ggj2013;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "ggj2013";
-		cfg.useGL20 = true;
-		cfg.width = 800;
-		cfg.height = 600;
-		
-		new LwjglApplication(new GameHolder(), cfg);
-		
-		
-	}
-	
-	
+
+    public static void main(String[] args) {
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.title = "ggj2013";
+        cfg.useGL20 = true;
+        cfg.width = 800;
+        cfg.height = 600;
+
+        new LwjglApplication(new GameHolder(), cfg);
+    }
 }

b990ea8b5f25953aa98548a2e7b1c6d7ce63fd6c
Alastair Lynn <arplynn@gmail.com>
2013-01-27 11:01:48 +0000

    Add the new embers

--- ggj2013/src/com/samphippen/games/ggj2013/CampfireSprite.java
+++ ggj2013/src/com/samphippen/games/ggj2013/CampfireSprite.java
@@ -9,15 +9,13 @@ import com.badlogic.gdx.math.Vector2;
 public class CampfireSprite implements GameObject {
 
     private Sprite mOnSprite;
-    private Sprite mOffSprite;
+    private final Sprite mOffSprite;
 
     private final List<Sprite> mOnSprites = new ArrayList<Sprite>();
 
-    private final List<Sprite> mOffSprites = new ArrayList<Sprite>();
-
     private boolean mOn = true;
     private Vector2 mPosition;
-    private final static Vector2 SPRITE_OFFSET = new Vector2(-56.0f, -64.0f);
+    private final static Vector2 SPRITE_OFFSET = new Vector2(-56.0f, -46.0f);
 
     private final Light mLight;
 
@@ -27,7 +25,7 @@ public class CampfireSprite implements GameObject {
         mLight.setOuterRadius(50.0f);
         mLight.setIntensity(0.7f);
         loadFrames(mOnSprites, 24, "campfire_large_", ".png");
-        loadFrames(mOffSprites, 20, "campfire_small_", ".png");
+        mOffSprite = GameServices.loadSprite("campfire-off.png");
     }
     
     public boolean getOn() {
@@ -66,13 +64,13 @@ public class CampfireSprite implements GameObject {
     public void update() {
         mFrameCount++;
         mOnSprite = mOnSprites.get(mFrameCount % mOnSprites.size());
-        mOffSprite = mOffSprites.get(mFrameCount % mOffSprites.size());
         boolean paniced = GameHolder.getInstance().mPulseG < 0.999;
         if (PlayerObject.getInstance().getPosition().dst(mPosition) < 40
                 && !paniced) {
             mOn = false;
             mLight.setInnerRadius(3.0f);
-            mLight.setOuterRadius(28.0f);
+            mLight.setOuterRadius(50.0f);
+            mLight.setIntensity(0.3f);
         }
     }
 
@@ -84,7 +82,7 @@ public class CampfireSprite implements GameObject {
             renderQueue.add(new SpriteRenderable(mOnSprite),
                     (int) mPosition.y + 10);
         } else {
-            mOffSprite.setPosition(drawPosition.x, drawPosition.y);
+            mOffSprite.setPosition(drawPosition.x, drawPosition.y + 5.0f);
             renderQueue.add(new SpriteRenderable(mOffSprite),
                     (int) mPosition.y + 10);
         }

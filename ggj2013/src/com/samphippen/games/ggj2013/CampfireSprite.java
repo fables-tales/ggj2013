25aeb7eb9033347aefa8d3d7e33bea01047ac04e
Alastair Lynn <arplynn@gmail.com>
2013-01-27 11:10:29 +0000

    First cut of the campfire transitions

--- ggj2013/src/com/samphippen/games/ggj2013/CampfireSprite.java
+++ ggj2013/src/com/samphippen/games/ggj2013/CampfireSprite.java
@@ -12,8 +12,10 @@ public class CampfireSprite implements GameObject {
     private final Sprite mOffSprite;
 
     private final List<Sprite> mOnSprites = new ArrayList<Sprite>();
+    private final List<Sprite> mTransitionSprites = new ArrayList<Sprite>();
 
     private boolean mOn = true;
+    private int mTransitionFrame = 0;
     private Vector2 mPosition;
     private final static Vector2 SPRITE_OFFSET = new Vector2(-56.0f, -46.0f);
 
@@ -24,7 +26,8 @@ public class CampfireSprite implements GameObject {
         mLight.setInnerRadius(10.0f);
         mLight.setOuterRadius(50.0f);
         mLight.setIntensity(0.7f);
-        loadFrames(mOnSprites, 24, "campfire_large_", ".png");
+        loadFrames(mOnSprites, 0, 24, "campfire_large_", ".png");
+        loadFrames(mTransitionSprites, 30, 59, "campfire_transition_", ".png");
         mOffSprite = GameServices.loadSprite("campfire-off.png");
     }
     
@@ -32,9 +35,9 @@ public class CampfireSprite implements GameObject {
         return mOn;
     }
 
-    public void loadFrames(List<Sprite> out, int count, String prefix,
+    public void loadFrames(List<Sprite> out, int start, int end, String prefix,
             String suffix) {
-        for (int i = 0; i < count; i++) {
+        for (int i = start; i <= end; i++) {
             String filename = "" + i;
             while (filename.length() < 3) {
                 filename = "0" + filename;
@@ -62,6 +65,9 @@ public class CampfireSprite implements GameObject {
 
     @Override
     public void update() {
+        if (!mOn) {
+            ++mTransitionFrame;
+        }
         mFrameCount++;
         mOnSprite = mOnSprites.get(mFrameCount % mOnSprites.size());
         boolean paniced = GameHolder.getInstance().mPulseG < 0.999;
@@ -77,14 +83,19 @@ public class CampfireSprite implements GameObject {
     @Override
     public void emitRenderables(RenderQueueProxy renderQueue) {
         Vector2 drawPosition = mPosition.cpy().add(SPRITE_OFFSET);
+        int zOrder = (int) mPosition.y + 10;
         if (mOn) {
             mOnSprite.setPosition(drawPosition.x, drawPosition.y);
-            renderQueue.add(new SpriteRenderable(mOnSprite),
-                    (int) mPosition.y + 10);
+            renderQueue.add(new SpriteRenderable(mOnSprite), zOrder);
         } else {
-            mOffSprite.setPosition(drawPosition.x, drawPosition.y + 5.0f);
-            renderQueue.add(new SpriteRenderable(mOffSprite),
-                    (int) mPosition.y + 10);
+            if (mTransitionFrame >= mTransitionSprites.size()) {
+                mOffSprite.setPosition(drawPosition.x, drawPosition.y + 5.0f);
+                renderQueue.add(new SpriteRenderable(mOffSprite), zOrder);
+            } else {
+                Sprite newSprite = mTransitionSprites.get(mTransitionFrame);
+                newSprite.setPosition(drawPosition.x, drawPosition.y);
+                renderQueue.add(new SpriteRenderable(newSprite), zOrder);
+            }
         }
     }
 

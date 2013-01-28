dfc2bb639d1a818ae05ed0be8d33c8e17dd9e024
Alastair Lynn <arplynn@gmail.com>
2013-01-28 11:25:19 +0000

    Remove individual sound disables

--- ggj2013/src/com/samphippen/games/ggj2013/sound/SoundManager.java
+++ ggj2013/src/com/samphippen/games/ggj2013/sound/SoundManager.java
@@ -1,7 +1,6 @@
 package com.samphippen.games.ggj2013.sound;
 
 import java.util.ArrayList;
-import java.util.Formatter;
 import java.util.List;
 
 import com.badlogic.gdx.Gdx;
@@ -10,7 +9,6 @@ import com.badlogic.gdx.audio.Sound;
 import com.samphippen.games.ggj2013.GameServices;
 
 public class SoundManager {
-    private static final int NUM_AMBIENTS = 5;
     private static final int NUM_FOOTSTEPS = 9;
     private final Sound mHeartBeat;
     private final Sound mThunk;
@@ -23,32 +21,14 @@ public class SoundManager {
     private long mLastThunkTick = 0;
     private long mLastScreechTick = 0;
     private long mLastStepTick = 0;
-    private final List<Sound> mAmbientSounds = new ArrayList<Sound>();
     private final List<Sound> mFootstepSounds = new ArrayList<Sound>();
 
-    private final boolean DISABLE_AMBIENT = true;
-    private final boolean DISABLE_HEARTBEAT = false;
-    private final boolean DISABLE_FOOTSTEPS = false;
-
     public SoundManager() {
-        if (!DISABLE_HEARTBEAT) {
-            mHeartBeat = Gdx.audio.newSound(Gdx.files
-                    .internal("bin/data/heartbeat-excited.wav"));
-        } else {
-            mHeartBeat = null;
-        }
-        if (!DISABLE_FOOTSTEPS) {
-            for (int i = 0; i < NUM_FOOTSTEPS; ++i) {
-                mFootstepSounds.add(Gdx.audio.newSound(Gdx.files
-                        .internal("bin/data/footsteps" + i + ".wav")));
-            }
-        }
-        if (!DISABLE_AMBIENT) {
-            for (int i = 0; i < NUM_AMBIENTS; ++i) {
-                mAmbientSounds.add(Gdx.audio.newSound(Gdx.files
-                        .internal(new Formatter().format(
-                                "bin/data/ambient_%d.wav", i).toString())));
-            }
+        mHeartBeat = Gdx.audio.newSound(Gdx.files
+                .internal("bin/data/heartbeat-excited.wav"));
+        for (int i = 0; i < NUM_FOOTSTEPS; ++i) {
+            mFootstepSounds.add(Gdx.audio.newSound(Gdx.files
+                    .internal("bin/data/footsteps" + i + ".wav")));
         }
         mThunk = Gdx.audio.newSound(Gdx.files.internal("bin/data/thunk.wav"));
         mScreech = Gdx.audio.newSound(Gdx.files
@@ -72,17 +52,11 @@ public class SoundManager {
     }
 
     public void beatHeart() {
-        if (DISABLE_HEARTBEAT) {
-            return;
-        }
         mHeartBeat.stop(mHeartBeatInstance);
         mHeartBeatInstance = mHeartBeat.play();
     }
 
     public void step() {
-        if (DISABLE_FOOTSTEPS) {
-            return;
-        }
         float pitch = (float) Math.pow(2.0,
                 0.1 * GameServices.sRng.nextGaussian());
         long currentTick = GameServices.getTicks();
